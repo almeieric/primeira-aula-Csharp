aae65594dd0f906cedcde590129594e6cf1af09f
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-08-19 17:47:15 -0300

    Tutorial csharp

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine("O Console.ReadKey responde ao obtêm o próximo caractere pressionado pelo usuário.");
+            Console.WriteLine("Já o Console.ReadLine ira ler uma linha inteira de inputs de caractere do usuario.");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
